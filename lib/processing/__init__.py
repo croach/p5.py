64241f801a4faa44ac261c136f4db4469d0b3a08
Christopher Roach <vthakr@gmail.com>
2013-09-07 23:10:01 -0700

    Added a couple of the random functions

--- lib/processing/__init__.py
+++ lib/processing/__init__.py
@@ -1,4 +1,5 @@
 import types
+import random as _random
 from functools import wraps
 
 from .sketch import Sketch
@@ -18,6 +19,46 @@ _sketch = Sketch()
 self = __import__(__name__)
 for func in _sketch.processing_functions:
     setattr(self, func.processing_name, func)
+
+# Random
+
+def random(*args):
+    """Generates random numbers.
+
+    Each time the random() function is called, it returns an unexpected value
+    within the specified range. If only one parameter is passed to the function,
+    it will return a float between zero and the value of the high parameter.
+    For example, random(5) returns values between 0 and 5 (starting at zero,
+    and up to, but not including, 5).
+
+    If two parameters are specified, the function will return a float with a
+    value between the two values. For example, random(-5, 10.2) returns values
+    starting at -5 and up to (but not including) 10.2. To convert a
+    floating-point random number to an integer, use the int() function.
+
+    Arguments:
+    low -- lower limit
+    high -- upper limit
+
+    Returns:
+    float
+    """
+    low = args[0] if len(args) > 1 else 0
+    high = args[1] if len(args) > 1 else args[0]
+    offset = _random.random() * (high - low)
+    return low + offset
+
+def random_seed(seed):
+    """Sets the seed value for random().
+
+    By default, random() produces different results each time the program is
+    run. Set the seed parameter to a constant to return the same pseudo-random
+    numbers each time the software is run.
+
+    Arguments:
+    seed -- seed value
+    """
+    _random.seed(seed)
 
 
 def size(width, height):
